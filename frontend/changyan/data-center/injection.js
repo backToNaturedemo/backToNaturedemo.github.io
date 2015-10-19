e8119283a3fad74135301af90071e2898462f349
郭豪(技术中心) <haoguo@sohu-inc.com>
2015-10-19 20:45:02 +0800

    update

--- frontend/changyan/data-center/injection.js
+++ frontend/changyan/data-center/injection.js
@@ -1,18 +1,25 @@
 $(function () {
+
+
+
     var common = function () {
-        var loading = function () {
+        var loading = function (fn) {
             var $press = $('<div class="cyan-data-press"></div>'),
-                $spinner = $('<div class="spinner"><div class="dot1"></div><div class="dot2"></div></div>');
-            $('body').append($press);
-            $('body').append($spinner);
+                $spinner = $('<div class="spinner"><div class="dot1"></div><div class="dot2"></div></div>'),
+                $body = $(window.document.body);
+            $body.append($press);
+            $body.append($spinner);
             window.setTimeout(function () {
                 $press.remove();
                 $spinner.remove();
                 $('body').fadeOut().fadeIn(500);
+                if ($.isFunction(fn)) {
+                    fn();
+                }
             }, 1000);
         };
-        loading();
 
+        loading();
     };
 
     var wap = function () {
@@ -23,16 +30,25 @@ $(function () {
         common();
     };
 
-
-    if ($('#SOHUCS').length) {
-        // 如果有接入畅言得话
-        if ($('#cy-cbox-wrapper').length) {
-            // wap站
-            wap();
-
-        } else {
-            // PC站
-            pc();
+    var init = function (data) {
+        if (data) {
+            alert(data);
         }
-    }
+        if ($('#SOHUCS').length) {
+            // 如果有接入畅言得话
+            if ($('#cy-cbox-wrapper').length) {
+                // wap站
+                wap();
+
+            } else {
+                // PC站
+                pc();
+            }
+        }
+    };
+
+    init();
+
+//    window._cyanData = init;
+
 });
---
+++ frontend/changyan/data-center/tpl.js
@@ -0,0 +1,6 @@
+/**
+ * Created by haoguo on 2015/10/19.
+ */
+$(function () {
+
+});

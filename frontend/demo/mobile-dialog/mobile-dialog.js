a90b2094989c860dd4094ec6a340443ff4536eb0
郭豪(技术中心) <haoguo@sohu-inc.com>
2016-01-07 15:16:36 +0800

    test

--- frontend/demo/mobile-dialog/mobile-dialog.js
+++ frontend/demo/mobile-dialog/mobile-dialog.js
@@ -4,31 +4,47 @@
  * @description 移动模态框
  */
 !(function () {
-    var Dialog = function (cfg) {
-        var $body = $('body');
+    var $body = $('body');
+    var Dialog = {
+        init: function (cfg) {
+            this.createPress();
+            this.renderDialog(cfg.tpl, cfg.onshowed); 
+        },
         // 创建遮罩
-        var createPress = function () {
-            var $press = $('<div class="cui-mobile-dialog-press"></div>');
+        createPress: function () {
+            var self = this,
+            $press = $('<div class="cui-mobile-dialog-press"></div>');
             $body.append($press);
-        };
-        
-        // 移除遮罩
-        var removePress = function () {
+            $press.on('click', function () {
+                self.close();
+            });
+        },
+        // 关闭模态框
+        close: function () {
             $('.cui-mobile-dialog-press').remove();
-        };
-
+            $('.cui-mobile-dialog-wrapper').remove();
+        },
         // 渲染模态框
-        var renderDialog = function () {
+        renderDialog: function (tpl, fn) {
             var $wrapper = $('<div class="cui-mobile-dialog-wrapper"></div>');
-            $wrapper.addClass('cui-mobile-dialog-pre').append(cfg.tpl);
-            $wrapper.show().removeClass('cui-mobile-dialog-pre');
+            $wrapper
+            .addClass('cui-mobile-dialog-pre')
+            .append(tpl)
+            .show()
+            .removeClass('cui-mobile-dialog-pre');
             $body.append($wrapper);
+            var win_height = $(window).height(),win_width = $(window).width(),
+            wrapper_height = $wrapper.height(),wrapper_width = $wrapper.width(),
+            top = (win_height - wrapper_height) / 2, left = (win_width - wrapper_width) / 2;
+            $wrapper.css('left', left).css('top', top);
+
+            if ($.isFunction(fn)) {
+                fn($wrapper);
+            }
             window.setTimeout(function () {
                 $wrapper.addClass('cui-mobile-dialog-active');
             },1);
-        };
-        createPress();
-        renderDialog();     
-    };   
+        }
+    };
     window.Dialog = Dialog;
 }());

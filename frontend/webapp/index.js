6053916a6f600a8ff9e0fc6d828d64c64afc7c5b
郭豪(技术中心) <haoguo@sohu-inc.com>
2016-04-27 18:04:43 +0800

    update

--- frontend/webapp/index.js
+++ frontend/webapp/index.js
@@ -1,6 +1,6 @@
 $(function () {
     var adaptive = function () {
-        $('.content').css('max-height', ($(window).height() - $('.header').height()) + 'px');
+        $('.content').css('max-height', ($(window).height() - $('.header').height() + $('.content-loading').height()) + 'px');
     };
     adaptive();
     window.index_adaptive = adaptive;
@@ -11,12 +11,34 @@ $(function () {
     $('.list-item').on('tap', function (e) {
         
     });
-
+    var pages = new Swiper('.swiper-container', {
+        onSlideChangeEnd: function (data) {
+            $('.header-tabs-ul li').eq(data.activeIndex).trigger('tap');
+        }
+    });
+    // debugger;
     (function () {
+        var isMoving = false;
         $('.header-tabs-ul').delegate('li', 'tap', function (e) {
-            var $this = $(this);
-            $('.header-tabs-ul li').removeClass('active');
-            $this.addClass('active');
+            var $this = $(this),$bottom_select = $('.bottom-select');
+            if (isMoving || $this.hasClass('active')) {
+                return;
+            }
+            pages.swipeTo($this.index());
+            isMoving = true;
+            $('.header-tabs-ul li').removeClass('active theme-blue');
+            $bottom_select.css('opacity', 1);
+            $this.addClass('theme-blue');
+            $bottom_select.css('-webkit-transform', 'translate3d('+$this.offset().left+'px,0,0)');
+            $bottom_select.one('webkitTransitionEnd', function (e) {
+                $bottom_select.css('opacity', 0);
+                $this.addClass('active');
+                isMoving = false;
+            });
+            
         });
     }());
+    $('.content').on('scroll', function () {
+        $('.content-loading').show();
+    });
 });
